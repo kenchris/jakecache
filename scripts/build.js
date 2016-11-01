163eb3d48b8a4d95c34ed3b2dad4b2f05292187b
ejb <mail@elliotbentley.com>
2016-11-01 08:22:47 -0400

    Transpile to ES5 during build process

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,15 @@
 var rollup = require('rollup')
+var babel = require('rollup-plugin-babel')
 
 var files = ['jakecache-sw.js', 'jakecache.js']
 
 files.forEach(function(file) {
 
   rollup.rollup({
-    entry: file
+    entry: file,
+      plugins: [
+        babel()
+      ]
   }).then( function ( bundle ) {
 
     bundle.write({
